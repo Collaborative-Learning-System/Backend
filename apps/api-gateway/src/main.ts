9229feac3c2af3377c60960bb1edec31d0c8b523
HarshaYohan <harshad.22@cse.mrt.ac.lk>
2025-10-11 19:49:52 +0530

    fix: add missing origin URL for CORS in API gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:8080'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'http://127.0.0.1:5500',
+      'http://localhost:8080',
+      'https://educollab-snowy.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
