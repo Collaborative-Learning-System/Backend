aa83ba3c55e24fa43a579ea84ed180aea975fce3
HarshaYohan <harshad.22@cse.mrt.ac.lk>
2025-10-14 05:03:49 +0530

    fix: update error messages for login credentials and enhance email validation in DTOs

--- apps/auth-service/src/dtos/login.dto.ts
+++ apps/auth-service/src/dtos/login.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsNotEmpty } from "class-validator";
 
 export class LoginDto {
-    @IsEmail()
+    @IsEmail({}, { message: 'Please provide a valid email address.' })
     @IsNotEmpty()
     email: string;
 
